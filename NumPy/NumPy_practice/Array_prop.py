c4ac92c0045cea9528fb2f969d690aa5f2da826d
Subhranshu Nanda <subhransu25112005@gmail.com>
2026-03-27 16:01:03 +0530

    change location

---
+++ NumPy/NumPy_practice/Array_prop.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+arr = np.array([
+    [10,20,30],
+    [40,50,60]
+])
+
+print("Array:\n", arr)
+
+print("Shape:", arr.shape)
+print("Dimensions:", arr.ndim)
+print("Size:", arr.size)
+print("Data Type:", arr.dtype)
