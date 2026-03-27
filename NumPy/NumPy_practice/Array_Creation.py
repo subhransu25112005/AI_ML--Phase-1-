2e4f49a460078495a7c89668671c5e6cc4a72cf3
Subhranshu Nanda <subhransu25112005@gmail.com>
2026-03-27 15:51:02 +0530

    Array creation

---
+++ NumPy/NumPy_practice/Array_Creation.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+# 1D array
+arr1 = np.array([10, 20, 30])
+
+# 2D array
+arr2 = np.array([
+    [1,2,3],
+    [4,5,6]
+])
+
+# Nested list
+nested = [
+    [7,8,9],
+    [10,11,12]
+]
+arr3 = np.array(nested)
+
+print("1D Array:", arr1)
+print("2D Array:\n", arr2)
+print("Nested Array:\n", arr3)
---
+++ NumPy/NumPy_practice/rle2.py
@@ -0,0 +1,13 @@
+#🌍 REAL LIFE MASTER PROGRAM
+
+#📊 Scenario: Store Sales Data
+
+import numpy as np
+
+sales = np.array([
+    [100, 200, 150],
+    [120, 210, 160],
+    [130, 220, 170]
+])
+
+print("Sales Data:\n", sales)
