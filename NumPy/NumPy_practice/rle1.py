a751e26fba827bdb27ded2276a37e17390e407bb
Subhranshu Nanda <subhransu25112005@gmail.com>
2026-03-27 15:36:03 +0530

    mark analysis using numpy array

---
+++ NumPy/NumPy_practice/rle1.py
@@ -0,0 +1,12 @@
+#RLE (REAL LIFE EXAMPLE)
+import numpy as np
+
+marks = np.array([85, 90, 78])
+
+final_marks = marks + 5
+
+improvement = (final_marks - marks)
+
+print("Original Marks:", marks)
+print("Final Marks:", final_marks)
+print("Improvement:", improvement)
